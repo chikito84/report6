1e8d7bb401f249e0bcb9eb881cdabca8ce8e0233
Tomoya Uchihara <e165728@ie.u-ryukyu.ac.jp>
2016-12-14 15:30:12 +0900

    Step1 finished

---
+++ src/main/java/jp/ac/uryukyu/ie/e165728/Hero.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e165728;
+
+public class Hero extends LivingThing{
+
+    public Hero(String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
+    }
+}
